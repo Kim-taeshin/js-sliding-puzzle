55471e0639294900ef89a5e3623187423776ed26
Kim-taeshin <againforever@naver.com>
2024-07-01 18:21:08 +0900

    빌드

--- dist/main.1e43358e.js
+++ dist/main.1e43358e.js
@@ -33646,7 +33646,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59308" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60504" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
